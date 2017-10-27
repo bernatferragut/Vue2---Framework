706d72c26f81a99e7786381e3a0af43a6e245b8e
Bernat Ferragut <elbernat@gmail.com>
2017-10-27 08:46:24 -0400

    Vue lists
    Vue Lists 1 - how they work

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
 // JS => Now our data is the single source of truth !!!
 
-mv = new Vue( {
+vm = new Vue( {
     el:'#area',
     data: {
         bands: [ 'TJAMC', 'RIDE', 'BOO RADLEYS', 'MVB'],
+    },
+    mounted() {
+        // selections
+        let bandInput = document.querySelector('input');
+        let buttonInput = document.querySelector('button');
+        // events
+        buttonInput.addEventListener('click', function() {
+            vm.bands.push(bandInput.value);
+            bandInput.value = '';
+        })
+        console.log('ready to go !');
     }
 });
-// console.log(mv);
-
 
