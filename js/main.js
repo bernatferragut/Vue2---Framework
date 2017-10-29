3a1d396ba37176fd04e311928ed26efa08ca8dbf
Bernat Ferragut <elbernat@gmail.com>
2017-10-29 14:52:43 -0400

    Tasks App
    Small app containing completed and uncompleted tasks

--- js/main.js
+++ js/main.js
@@ -3,20 +3,28 @@
 vm = new Vue( {
     el:'#vue',
     data: {
-        title: 'title canged with javascript',
-        content: 'This is the content',
-        isLoading: false,
+        message: 'Bonjour Bernat!',
+        tasks: [
+            { description: 'Finish Vue course', completed: true },
+            { description: 'Clean Floor with Cleaner', completed: false },
+            { description: 'Clean Toilet', completed: false },
+            { description: 'Think about weekly classes', completed: false },
+            { description: 'Check the EOS registration thing', completed: false },
+            { description: 'Relax reading Computer Philosophy', completed: true },
+        ]
     },
     methods: {
-        action1: function() {
-            console.log('you just cliced me!');
-            this.isLoading = !this.loading;
+        
+    },
+    computed: {
+        reversedMessage: function() {
+            return this.message.split('').reverse().join('');
+        },
+        completedTasks: function() {
+            return this.tasks.filter( task => task.completed);
         }
     },
     mounted() {
         console.log('Vue is ready!!!');
-        // this.isLoading = false;
-        console.log(this.isLoading);
     }
 });
-
