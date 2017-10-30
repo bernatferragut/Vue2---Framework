592e1fa3f26f497e1ae95265ba987a6336cc6bff
Bernat Ferragut <elbernat@gmail.com>
2017-10-30 15:24:47 -0400

    Canvas color explosion
    two new amazing canvas color explosions

--- js/main.js
+++ js/main.js
@@ -1,30 +1,18 @@
-// JS => Now our data is the single source of truth !!!
+// Global View component creation
+Vue.component('band',  {
+    template: '<li> {{ message }} </li>',
 
+    data() {
+        return {
+            message: 'foobar';
+        }
+    }
+});
+
+// Vue Object
 vm = new Vue( {
     el:'#vue',
     data: {
-        message: 'Bonjour Bernat!',
-        tasks: [
-            { description: 'Finish Vue course', completed: true },
-            { description: 'Clean Floor with Cleaner', completed: false },
-            { description: 'Clean Toilet', completed: false },
-            { description: 'Think about weekly classes', completed: false },
-            { description: 'Check the EOS registration thing', completed: false },
-            { description: 'Relax reading Computer Philosophy', completed: true },
-        ]
-    },
-    methods: {
-        
-    },
-    computed: {
-        reversedMessage: function() {
-            return this.message.split('').reverse().join('');
-        },
-        completedTasks: function() {
-            return this.tasks.filter( task => task.completed);
-        }
-    },
-    mounted() {
-        console.log('Vue is ready!!!');
+        bandsList: ['TJAMC','LOOP','RIDE'],
     }
 });
